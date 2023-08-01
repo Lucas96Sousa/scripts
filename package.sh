25775a5b267d917fd0e4ddcfe8ccbc78b77d3b24
Lucas Sousa <lucas996oliveira@gmail.com>
2023-08-01 19:27:06 -0300

    Feature: include vscode

--- package.sh
+++ package.sh
@@ -32,6 +32,9 @@ arch_install() {
     # GH
     yay github-cli
 
+    # CODE
+    yes | sudo pacman -Sy code
+
     #1
     curl -sSO https://downloads.1password.com/linux/tar/stable/x86_64/1password-latest.tar.gz
     sudo tar -xf 1password-latest.tar.gz
