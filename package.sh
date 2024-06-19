e48a77bfaea25d69699019780852cd97525b12a3
Lucas Sousa <Lucas996oliveira@gmail.com>
2024-06-18 23:05:11 -0300

    Update package.sh

--- package.sh
+++ package.sh
@@ -156,11 +156,21 @@ Fedora_Install(){
             echo "$filename exists."
         else
             echo "$filename does not exists need to install"
-            sudo dnf install tmux fish -y
+            sudo dnf install kitty tmux fish -y
         fi
     done
 
     #Programing
+
+	#Docker
+ 	sudo dnf -y install dnf-plugins-core
+  	sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+	sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+ 	sudo systemctl start docker 
+  	sudo systemctl enable docker
+	
     #Java
     sudo dnf install java-17-openjdk java-17-openjdk-devel 
 
@@ -170,12 +180,10 @@ Fedora_Install(){
     #Cpp
     sudo dnf install cmake g++ make
 
-
+    #Elixir
+    sudo dnf install erlang elixir 
     #Golang
     sudo dnf install go
-
-    #Flatpak 
-    flatpak install flathub com.discordapp.Discord io.beekeeperstudio.Studio io.dbeaver.DBeaverCommunity com.github.sdv43.whaler
 
     #TPM and HOMEBREW(FISH)
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
